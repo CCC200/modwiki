b505ae8854bb857b1fe5dabff6947965fbe866fb
CCC200 <11540770+CCC200@users.noreply.github.com>
2025-09-22 17:30:07 -0400

    Move parsing and better checks to avoid bad data

--- py/parser.py
+++ py/parser.py
@@ -21,39 +21,39 @@ def build_dex(mod='base', dexOverride=False):
     prevo = False
     evos = False
     for line in ts_data:
-        if line.find('export const') > -1:
+        if line.find('export const') > -1 or line.find('\t\t}') > -1:
             continue
         elif line.find(': {') > -1 and in_mon == False: # open object
             in_mon = True
             name = line.split(':')[0].strip()
-        elif in_mon and line.find('name') > -1: # explicit name
+        elif in_mon and line.find('name:') > -1: # explicit name
             dname = line[line.find('"')+1:]
             dname = dname[:dname.find('"')]
-        elif in_mon and line.find('baseStats') > -1: # bst
+        elif in_mon and line.find('baseStats:') > -1: # bst
             bst_s = line[line.find('{')+1:line.find('}')].split(',')
             for i in range(len(bst_s)):
                 bst_s[i] = '"' + bst_s[i]
                 bst_s[i] = bst_s[i].replace(' ', '').replace(':', '":')
             bst_s = '{' + ','.join(bst_s) + '}'
             bst = json.loads(bst_s)
-        elif in_mon and line.find('types') > -1: # type
+        elif in_mon and line.find('types:') > -1: # type
             types_s = line[line.find('[')+1:line.find(']')].replace(' ', '').replace('"', '')
             types = types_s.split(',')
-        elif in_mon and line.find('abilities') > -1: # abilities
+        elif in_mon and line.find('abilities:') > -1: # abilities
             abil_s = line[line.find('{')+1:line.find('}')]
             abil_s = abil_s.replace('0:', '"0":').replace('1:', '"1":').replace('H:', '"H":').replace('S:', '"S":')
             abil_s = abil_s.split(',')
             abil_s = '{' + ','.join(abil_s) + '}'
             abil = json.loads(abil_s)
-        elif in_mon and line.find('prevo') > -1: # pre-evolutions
+        elif in_mon and line.find('prevo:') > -1: # pre-evolutions
             prevo = line[line.find('"')+1:]
             prevo = prevo[:prevo.find('"')]
-        elif in_mon and line.find('evos') > -1:
+        elif in_mon and line.find('evos:') > -1:
             evos_s = line[line.find('[')+1:line.find(']')].replace(' ', '').replace('"', '')
             evos = evos_s.split(',')
-        elif in_mon and line.find('isCosmeticForme') > -1: # special for polished formes
+        elif in_mon and line.find('isCosmeticForme:') > -1: # special for polished formes
             is_cosmetic = True
-        elif in_mon and line.find('\t},') > -1: # close object
+        elif in_mon and line.find('\t},') > -1 or in_mon and line.find('    },') > -1: # close object
             in_mon = False
             if is_cosmetic:
                 is_cosmetic = False
@@ -93,6 +93,7 @@ def build_learnset(dex, mod='base'):
     data_file = open(f'_cache/{mod}/learnsets.ts')
     ts_data = data_file.read()
     data_file.close()
+    ts_data = ts_data.replace('hijumpkick', 'highjumpkick') # polished override
     # parse learnset
     for mon, data in dex.items():
         slice_data = ts_data[ts_data.find(mon):]
@@ -105,3 +106,95 @@ def build_learnset(dex, mod='base'):
         slice_data.pop() # remove empty last element
         dex[mon]['learnset'] = slice_data
     return dex
+
+def build_moves(mod='base', moveOverride=False):
+    data_file = open(f'_cache/{mod}/moves.ts')
+    ts_data = data_file.readlines()
+    data_file.close()
+    # parse moves
+    moves = {}
+    if moveOverride:
+        moves = moveOverride
+    movelist = {
+        "moves": []
+    }
+    in_move = False
+    non_standard = False
+    name = ''
+    dname = False
+    cat = False
+    acc = False
+    bp = False
+    pp = False
+    type = False
+    desc = False
+    for line in ts_data:
+        if line.find('export const') > -1 or line.find('\t\t}') > -1:
+            continue
+        elif line.find(': {') > -1 and in_move == False: # open object
+            in_move = True
+            name = line.split(':')[0].strip().replace('"', '')
+            if name == 'hijumpkick': # polished override
+                name = 'highjumpkick'
+        elif in_move and line.find('name:') > -1: # explicit name
+            dname = line[line.find('"')+1:]
+            dname = dname[:dname.find('"')]
+        elif in_move and line.find('category:') > -1: # phys/spec category
+            cat = line[line.find('"')+1:]
+            cat = cat[:cat.find('"')]
+        elif in_move and line.find('accuracy:') > -1: # accuracy
+            acc_s = line[line.find(':')+1:line.find(',')]
+            acc = acc_s.strip()
+            acc = '100' if acc == 'true' else acc
+        elif in_move and line.find('basePower:') > -1: # bp
+            bp_s = line[line.find(':')+1:line.find(',')]
+            bp = bp_s.strip()
+        elif in_move and line.find('pp:') > -1: # pp
+            pp_s = line[line.find(':')+1:line.find(',')]
+            pp = pp_s.strip()
+        elif in_move and line.find('type:') > -1: # type
+            type = line[line.find('"')+1:]
+            type = type[:type.find('"')]
+        elif in_move and line.find('shortDesc:') > -1:
+            desc = line[line.find('"')+1:]
+            desc = desc[:desc.find('"')]
+        elif in_move and moveOverride and line.find('isNonstandard:') > -1:
+            non_standard = True
+        elif in_move and line.find('\t},') > -1 or in_move and line.find('    },') > -1: # close object
+            in_move = False
+            if non_standard:
+                non_standard = False
+                continue
+            movelist['moves'].append(name)
+            # enter or override fields
+            if name not in moves:
+                moves[name] = {}
+            if dname:
+                moves[name]['name'] = dname
+                dname = False
+            if cat:
+                moves[name]['category'] = cat
+                cat = False
+            if type:
+                moves[name]['type'] = type
+                type = False
+            if acc:
+                moves[name]['accuracy'] = acc
+                acc = False
+            if bp:
+                moves[name]['bp'] = bp
+                bp = False
+            if pp:
+                moves[name]['pp'] = pp
+                pp = False
+            if desc:
+                moves[name]['desc'] = desc
+                desc = False
+    if moveOverride: # clear unused entries
+        to_del = []
+        for move, data in moves.items():
+            if move not in movelist['moves']:
+                to_del.append(move)
+        for move in to_del:
+            del moves[move]
+    return moves, movelist
